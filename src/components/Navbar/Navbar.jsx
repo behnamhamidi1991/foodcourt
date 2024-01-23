a3e607918169a6c3ec0aefe44f2491ebe0642f3d
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-24 00:42:08 +0330

    Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,42 @@
 import React from "react";
 import "./navbar.scss";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <h1>عنوان وبسایت</h1>
-      <p>این متن صرفاً یک تست می باشد</p>
+      <ul className="navUl">
+        <li className="navItem">
+          {" "}
+          <Link href="/" className="navLink">
+            صفحه اصلی
+          </Link>{" "}
+        </li>
+        <li className="navItem">
+          {" "}
+          <Link href="/" className="navLink">
+            وبلاگ{" "}
+          </Link>{" "}
+        </li>
+        <li className="navItem">
+          {" "}
+          <Link href="/" className="navLink">
+            رویدادها{" "}
+          </Link>{" "}
+        </li>
+        <li className="navItem">
+          {" "}
+          <Link href="/" className="navLink">
+            درباره ما{" "}
+          </Link>{" "}
+        </li>
+        <li className="navItem">
+          {" "}
+          <Link href="/" className="navLink">
+            تماس با ما{" "}
+          </Link>{" "}
+        </li>
+      </ul>
     </div>
   );
 };

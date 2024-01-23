a895dc5e1cef0b70f9637b1343cbd32b9ceb6463
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-24 00:19:34 +0330

    Test

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1>عنوان وبسایت</h1>
+      <p>این متن صرفاً یک تست می باشد</p>
+    </div>
+  );
+};
+
+export default Navbar;

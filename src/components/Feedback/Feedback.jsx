ec7a06bf9a0d57b132788f442794052d1c6b50ac
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-25 16:03:59 +0330

    Commit

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "feedback.scss";
+import Image from "next/image";
+
+const Feedback = () => {
+  return (
+    <div className="feedback-container">
+      <div className="titleBox"></div>
+
+      <div className="feedback"></div>
+    </div>
+  );
+};
+
+export default Feedback;

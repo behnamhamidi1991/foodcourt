8ac0bb3d4fcacbb6cbc889b3c984a537b2569e3a
behnamhamidi1991 <eslp91@hotmail.com>
2024-01-25 15:37:57 +0330

    Blog Section Done

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -7,13 +7,24 @@ import Link from "next/link";
 const Blog = () => {
   return (
     <div className="blog-container">
-      <div className="blog-header"></div>
+      <div className="blog-header">
+        <h2>وبلاگ</h2>
+        <p>
+          توی وبلاگ ما کلی مطالب متنوع و مفید پیدا می کنید، برای خواندن مطالب
+          بیشتر به صفحه وبلاگ ما مراجعه کنید
+        </p>
+      </div>
 
       <div className="post-container">
         {blogPost.map((post) => (
           <div key={post.id} className="post-box">
             <div className="img-container">
-              <Image src={post.blogImg} width={300} alt={post.title} />
+              <Image
+                className="post-image"
+                src={post.blogImg}
+                width={300}
+                alt={post.title}
+              />
             </div>
             <div className="blog-content">
               <div className="details">
@@ -21,8 +32,8 @@ const Blog = () => {
                   <h3>{post.title}</h3>
                 </div>
                 <div className="author-date">
-                  <p>{post.date}</p>
-                  <p>{post.author}</p>
+                  <p className="date">{post.date}</p>
+                  <p className="author">{post.author}</p>
                 </div>
               </div>
 
